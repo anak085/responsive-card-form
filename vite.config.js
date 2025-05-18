7342fceb5fe0745ea7d719a216f2989e83009507
Ana Kesia <anakesia0945@gmail.com>
2025-05-18 18:04:42 -0300

    Atualiza vite.config.js para configuração HTTPS somente em dev

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,23 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import fs from "fs";
-import path from "path";
-import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from 'vite';
+import fs from 'fs';
+import path from 'path';
 
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  server: {
-    https: {
-      key: fs.readFileSync(path.resolve(__dirname, "localhost-key.pem")),
-      cert: fs.readFileSync(path.resolve(__dirname, "localhost.pem")),
-    },
-    host: true,
-    port: 5173,
-  },
+export default defineConfig(({ mode }) => {
+  const isLocal = mode === 'development';
+  const config = {};
+
+  if (isLocal) {
+    try {
+      config.server = {
+        https: {
+          key: fs.readFileSync(path.resolve(__dirname, 'localhost-key.pem')),
+          cert: fs.readFileSync(path.resolve(__dirname, 'localhost-cert.pem')),
+        },
+      };
+    } catch (err) {
+      console.warn('Certificados HTTPS não encontrados, rodando sem HTTPS.');
+    }
+  }
+
+  return config;
 });
